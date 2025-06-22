df5617aa2d66be344520a243482ff727c7bfd951
Mateus <mpdasilvatreze@gmail.com>
2025-06-22 10:48:21 -0300

    feat(main): Move e exporta icones da sessão "Stack"

--- assets/icons/iconExporter.ts
+++ assets/icons/iconExporter.ts
@@ -1,15 +1,83 @@
+// theme icons
+import Moon from './theme_icons/moon.svg'
+import Sun from './theme_icons/sun.svg'
+
+// services icons
 import Ai from './service_icons/ai.svg';
 import Binary from './service_icons/binary.svg';
 import CardinalPoints from './service_icons/cardinal-points.svg';
 import Support from './service_icons/support.svg'
-import Moon from './theme_icons/moon.svg'
-import Sun from './theme_icons/sun.svg'
+
+// stack icons
+import AWS from './stack_icons/AWS.svg';
+import Bash from './stack_icons/Bash.svg';
+import Cloudflare from './stack_icons/Cloudflare.svg';
+import Composer from './stack_icons/Composer.svg';
+import CSS3 from './stack_icons/CSS3.svg';
+import Docker from './stack_icons/Docker.svg';
+import Git from './stack_icons/Git.svg';
+import Go from './stack_icons/Go.svg';
+import Grafana from './stack_icons/Grafana.svg';
+import Jest from './stack_icons/Jest.svg';
+import Jira from './stack_icons/Jira.svg';
+import Linux from './stack_icons/Linux.svg';
+import MaterialUI from './stack_icons/Material-UI.svg';
+import MySQL from './stack_icons/MySQL.svg';
+import NextJS from './stack_icons/Next.js.svg';
+import NGINX from './stack_icons/NGINX.svg';
+import NodeJS from './stack_icons/Node.js.svg';
+import NPM from './stack_icons/NPM.svg';
+import Pandas from './stack_icons/Pandas.svg';
+import PHP from './stack_icons/PHP.svg';
+import PostgreSQL from './stack_icons/PostgreSQL.svg';
+import Postman from './stack_icons/Postman.svg';
+import Prometheus from './stack_icons/Prometheus.svg';
+import Python from './stack_icons/Python.svg';
+import ReactIcon from './stack_icons/React.svg';
+import SSH from './stack_icons/SSH.svg';
+import Swagger from './stack_icons/Swagger.svg';
+import TypeScript from './stack_icons/TypeScript.svg';
+import VSCode from './stack_icons/Visual-Studio-Code-(VS-Code).svg';
+import Webpack from './stack_icons/Webpack.svg';
+import Zend from './stack_icons/Zend-Framework.svg';
+
 
 export const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
+  'moon.svg': Moon,
+  'sun.svg': Sun,
   'ai.svg': Ai,
   'binary.svg': Binary,
   'cardinal-points.svg': CardinalPoints,
   'support.svg': Support,
-  'moon.svg': Moon,
-  'sun.svg': Sun
+  'AWS.svg': AWS,
+  'Bash.svg': Bash,
+  'Cloudflare.svg': Cloudflare,
+  'Composer.svg': Composer,
+  'CSS3.svg': CSS3,
+  'Docker.svg': Docker,
+  'Git.svg': Git,
+  'Go.svg': Go,
+  'Grafana.svg': Grafana,
+  'Jest.svg': Jest,
+  'Jira.svg': Jira,
+  'Linux.svg': Linux,
+  'Material-UI.svg': MaterialUI,
+  'MySQL.svg': MySQL,
+  'Next.js.svg': NextJS,
+  'NGINX.svg': NGINX,
+  'Node.js.svg': NodeJS,
+  'NPM.svg': NPM,
+  'Pandas.svg': Pandas,
+  'PHP.svg': PHP,
+  'PostgreSQL.svg': PostgreSQL,
+  'Postman.svg': Postman,
+  'Prometheus.svg': Prometheus,
+  'Python.svg': Python,
+  'React.svg': ReactIcon,
+  'SSH.svg': SSH,
+  'Swagger.svg': Swagger,
+  'TypeScript.svg': TypeScript,
+  'Visual-Studio-Code-(VS-Code).svg': VSCode,
+  'Webpack.svg': Webpack,
+  'Zend-Framework.svg': Zend
 };
