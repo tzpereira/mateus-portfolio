1f53be6985c298d35f9b130d57a386bca03b4c5f
Mateus <mpdasilvatreze@gmail.com>
2025-06-23 22:45:11 -0300

    feat(main): Altera icones

--- assets/icons/iconExporter.ts
+++ assets/icons/iconExporter.ts
@@ -13,22 +13,20 @@ import AWS from './stack_icons/AWS.svg';
 import Bash from './stack_icons/Bash.svg';
 import Cloudflare from './stack_icons/Cloudflare.svg';
 import Composer from './stack_icons/Composer.svg';
-import CSS3 from './stack_icons/CSS3.svg';
 import Docker from './stack_icons/Docker.svg';
 import Git from './stack_icons/Git.svg';
 import Go from './stack_icons/Go.svg';
 import Grafana from './stack_icons/Grafana.svg';
-import Jest from './stack_icons/Jest.svg';
-import Jira from './stack_icons/Jira.svg';
 import Linux from './stack_icons/Linux.svg';
-import MaterialUI from './stack_icons/Material-UI.svg';
 import MySQL from './stack_icons/MySQL.svg';
 import NextJS from './stack_icons/Next.js.svg';
 import NGINX from './stack_icons/NGINX.svg';
 import NodeJS from './stack_icons/Node.js.svg';
 import NPM from './stack_icons/NPM.svg';
-import Pandas from './stack_icons/Pandas.svg';
+import Polars from './stack_icons/Polars.svg';
+import Fiber from './stack_icons/Fiber.svg';
 import PHP from './stack_icons/PHP.svg';
+import Javascript from './stack_icons/Javascript.svg';
 import PostgreSQL from './stack_icons/PostgreSQL.svg';
 import Postman from './stack_icons/Postman.svg';
 import Prometheus from './stack_icons/Prometheus.svg';
@@ -53,21 +51,16 @@ export const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = 
   'Bash.svg': Bash,
   'Cloudflare.svg': Cloudflare,
   'Composer.svg': Composer,
-  'CSS3.svg': CSS3,
   'Docker.svg': Docker,
   'Git.svg': Git,
   'Go.svg': Go,
   'Grafana.svg': Grafana,
-  'Jest.svg': Jest,
-  'Jira.svg': Jira,
   'Linux.svg': Linux,
-  'Material-UI.svg': MaterialUI,
   'MySQL.svg': MySQL,
   'Next.js.svg': NextJS,
   'NGINX.svg': NGINX,
   'Node.js.svg': NodeJS,
   'NPM.svg': NPM,
-  'Pandas.svg': Pandas,
   'PHP.svg': PHP,
   'PostgreSQL.svg': PostgreSQL,
   'Postman.svg': Postman,
@@ -77,7 +70,10 @@ export const iconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = 
   'SSH.svg': SSH,
   'Swagger.svg': Swagger,
   'TypeScript.svg': TypeScript,
-  'Visual-Studio-Code-(VS-Code).svg': VSCode,
+  'VSCode.svg': VSCode,
   'Webpack.svg': Webpack,
-  'Zend-Framework.svg': Zend
+  'Zend-Framework.svg': Zend,
+  'Polars.svg': Polars,
+  'Fiber.svg': Fiber,
+  'Javascript.svg': Javascript
 };
