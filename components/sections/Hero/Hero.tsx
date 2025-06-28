bd8c284dbfd0ec4798075aa2cd2d975238315059
Mateus <mpdasilvatreze@gmail.com>
2025-06-27 21:15:00 -0300

    feat(main): change hero layout and style

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -9,15 +9,25 @@ import initTranslations from '@/app/i18n';
 
 // components
 import { Header } from '@/components/layout/Header';
+import { ScrollIndicator } from '@/components/ui/ScrollIndicator';
 
 export default async function Hero({ locale }: HeroProps) {
-  const { t } = await initTranslations(locale, ['hero']); 
+  const { t } = await initTranslations(locale, ['hero']);
 
   return (
-    <section className="section hero">
-      <div className="hero__frame">
+    <section id="hero" className="section hero">
+      <div className="hero__header-frame">
         <Header locale={locale} />
-        <span className="hero__note">{t('note')}</span>
+      </div>
+      <div className="hero__frame">
+        <div className="hero__content">
+          <h1 className="hero__title">
+            MAT<span className="hero__title--break">EUS</span>
+          </h1>
+          <p className="hero__description">{t('title')}</p>
+          <span className="hero__note">{t('note')}</span>
+        </div>
+        <ScrollIndicator targetId={'services'} />
       </div>
     </section>
   );
