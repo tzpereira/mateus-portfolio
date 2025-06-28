63a6f378882055156ba6299bb220cdc682937657
Mateus <mpdasilvatreze@gmail.com>
2025-06-27 21:06:04 -0300

    feat(main):  Add burger menu to header

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,26 +1,55 @@
+'use client';
+
+// styles
 import './index.scss';
+
+// types
 import { HeaderProps } from './types';
+
+// react
+import React, { useState, useEffect } from 'react';
+
+// i18next
+import type { TFunction } from 'i18next';
 import initTranslations from '@/app/i18n';
 
+// custom components
+import { BurgerMenu } from '@/components/ui/BurgerMenu';
 import { ThemeChanger } from '@/components/ui/ThemeChanger';
 import { LanguageChanger } from '@/components/ui/LanguageChanger';
 
-export default async function Header({ locale }: HeaderProps) {
-  const { t } = await initTranslations(locale, ['header']);
+export default function Header({ locale }: HeaderProps) {
+  const [t, setT] = useState<TFunction | null>(null);
+
+  useEffect(() => {
+    initTranslations(locale, ['header']).then(({ t }) => {
+      setT(() => t);
+    });
+  }, [locale]);
+
+  if (!t) return null;
+
+  function scrollToSection(id: string) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  const menuItems = [
+    { label: t('services'), onClick: () => scrollToSection('services') },
+    { label: t('work'), onClick: () => scrollToSection('work') },
+    { label: t('stack'), onClick: () => scrollToSection('stack') },
+    { label: t('contact'), onClick: () => scrollToSection('contact') },
+  ];
 
   return (
     <header className="header">
-        <h1 className="header__logo">{t('logo')}</h1>
-      <nav className="header__nav">
-        <ul className="header__menu">
-          <li>{t('services')}</li>
-          <li>{t('work')}</li>
-          <li>{t('stack')}</li>
-          <li>{t('contact')}</li>
-        </ul>
+      <BurgerMenu menuItems={menuItems} />
+      <div className="header__actions">
         <ThemeChanger />
         <LanguageChanger />
-      </nav>
+      </div>
     </header>
   );
 }
