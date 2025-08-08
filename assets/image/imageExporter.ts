f7a69538cc30b302476251470d1e418302cfcbaf
Mateus <99037253+mateuspdasilva@users.noreply.github.com>
2025-08-08 00:38:17 -0300

    fix(work): update project details and add Bitcoin Price Forecasting image

--- assets/image/imageExporter.ts
+++ assets/image/imageExporter.ts
@@ -3,7 +3,9 @@ import type { StaticImageData } from 'next/image';
 
 // Conteúdo
 import ApiBench from './work_image/api-bench-cli.png';
+import BitcoinPriceForecasting from './work_image/bitcoin-price-forecasting.png';
 
 export const imageMap: Record<string, StaticImageData> = {
   'api-bench.png': ApiBench,
+  'bitcoin-price-forecasting.png': BitcoinPriceForecasting,
 };
